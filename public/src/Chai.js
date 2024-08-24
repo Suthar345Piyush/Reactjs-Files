ed95fa5261016168017c025a0308b9b39a2f980a
Suthar345Piyush <piyushsuthar524@gmail.com>
2024-08-25 00:45:44 +0530

    first commit

---
+++ public/src/Chai.js
@@ -0,0 +1,11 @@
+function Chai(){
+
+  return(
+
+
+    <h2>Chai in react</h2>
+  )
+}
+// file name and components name must be in the capitalized form ,file name is not mandatory
+
+export default Chai;
